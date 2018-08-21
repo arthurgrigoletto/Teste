ea5d56252ba7ac2c8baca4fc641a78d5c5ef639d
Arthur Grigoletto Navarro de Lim <arthur.grigoletto@ibm.com>
2018-08-21 11:17:03 -0300

    Add Education & Experience Form and Functionality done

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,7 +4,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
-  GET_ERRORS
+  GET_ERRORS,
+  SET_CURRENT_USER
 } from './types';
 
 // Get current profile
@@ -38,6 +39,49 @@ export const createProfile = (profileData, history) => dispatch => {
       })
     );
 };
+
+// Add Experience
+export const addExperience = (expData, history) => dispatch => {
+  axios.post('/api/profile/experience', expData)
+  .then(res => history.push('/dashboard'))
+  .catch(err => 
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  )
+}
+
+// Add Education
+export const addEducation = (eduData, history) => dispatch => {
+  axios.post('/api/profile/education', eduData)
+  .then(res => history.push('/dashboard'))
+  .catch(err => 
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  )
+}
+
+// Delete Account & Profile
+export const deleteAccount = () => dispatch => {
+  if(window.confirm('Are you sure? This can NOT be undone!')){
+    axios.delete('/api/profile')
+    .then(res => 
+      dispatch({
+        type: SET_CURRENT_USER,
+        payload: {}
+      })
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+  }
+}
 
 // Profile loading
 export const setProfileLoading = () => {
